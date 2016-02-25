168259813dd18e2e3dbef585eb450ad33ccf5aff
Brian Bird <birdb@lanecc.edu>
2016-02-25 11:49:51 -0800

    Finished the essentials for Authorization
    Restricted the Auth/Index controller method

--- BookStoreDemo/Controllers/AuthController.cs
+++ BookStoreDemo/Controllers/AuthController.cs
@@ -19,6 +19,7 @@ namespace BookStore.Controllers
         UserManager<AppUser> userManager = new UserManager<AppUser>(
                new UserStore<AppUser>(new BookStoreDemoContext()));
 
+        [Authorize(Roles="Admin, UberAdmin")]
         public ActionResult Index()
         {
             var roles = db.Roles.ToList();
@@ -166,5 +167,87 @@ namespace BookStore.Controllers
                 return View();
             }
         }
+
+        public ActionResult Delete(string RoleName)
+        {
+            var thisRole = db.Roles.Where(r => r.Name.Equals(
+                RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            db.Roles.Remove(thisRole);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Roles/Edit/5
+        public ActionResult EditRole(string roleName)
+        {
+            var thisRole = db.Roles.Where(r => r.Name.Equals(roleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+            return View(thisRole);
+        }
+
+        //
+        // POST: /Roles/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditRole(Microsoft.AspNet.Identity.EntityFramework.IdentityRole role)
+        {
+            try
+            {
+                db.Entry(role).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult ManageUserRoles()
+        {
+            // prepopulat roles for the view dropdown
+            var list = db.Roles.OrderBy(r => r.Name).ToList().Select(rr =>
+
+            new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RoleAddToUser(string UserName, string RoleName)
+        {
+            AppUser user = db.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            userManager.AddToRole(user.Id, RoleName);
+
+            ViewBag.ResultMessage = "Role created successfully !";
+
+            // prepopulat roles for the view dropdown
+            var list = db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+
+            return View("ManageUserRoles");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetRoles(string UserName)
+        {
+            if (!string.IsNullOrWhiteSpace(UserName))
+            {
+                AppUser user = db.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+
+                ViewBag.RolesForThisUser = userManager.GetRoles(user.Id);
+
+                // prepopulat roles for the view dropdown
+                var list = db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+                ViewBag.Roles = list;
+            }
+
+            return View("ManageUserRoles");
+        }
+
 	}
 }
