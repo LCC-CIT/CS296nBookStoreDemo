10cd815e1d1c339883c127506c7c02c55615169e
Brian Bird <birdb@lanecc.edu>
2016-03-01 22:24:59 -0800

    Added user and role to seed
    Creates a user and adds the Admin role to it.

--- BookStoreDemo/Controllers/AuthController.cs
+++ BookStoreDemo/Controllers/AuthController.cs
@@ -3,9 +3,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
